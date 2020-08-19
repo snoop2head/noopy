495dd142de0fca878d3db2cf865adc06b9a7746e
noopy <young_ahn@yonsei.ac.kr>
2020-08-19 11:09:36 +0900

    update gatsby meta data

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `noopy`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  author: `snoop2head`,
+  introduction: `📝🐍🦀`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
+    github: `snoop2head`, // Your GitHub account
+    medium: '',
+    facebook: '',
     facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    linkedin: `young-jin-ahn-b28616117`, // Your LinkedIn account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`blog`, `data`, `sc`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'snoop2head/noopy', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
